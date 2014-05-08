cd8686a53797ede8523b83a061fc902694860a09
Sean Manhertz <sean.manhertz@gmail.com>
2014-05-08 12:38:22 -0600

    Created a function to take the current url req and return the next url in the list to simulate the Magellan tour

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 
 app.set('view engine','jade');
@@ -22,27 +23,50 @@ var urlPaths = [
 	'/philippines'
 ];
 
+var findDestination = function (req) {
+	var currentPage = req.path;
+	console.log(currentPage);
+	console.log(urlPaths);
+	for(var i = 0; i < urlPaths.length; i++){
+	// console.log(urlPaths[i]);
+		if(urlPaths[i] === currentPage ) {
+			// ??
+			// currentPage = urlPaths[i];
+			var destinationPath = urlPaths[i + 1];
+			// return destinationPath;
+		}
+	};
+		console.log(destinationPath);
+};
+// 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
+	findDestination(req);
 	res.render('seville', {pageName:location});
+	
 });
 
 // URL path
 app.get('/canaryislands', function(req, res) {
-	// page name excluding the .jade
+	findDestination(req);
 	res.render('canaryislands', {pageName:location});
 });
 
 app.get('/capeverde', function(req, res) {
+	findDestination(req);
+
 	res.render('capeverde', {pageName:location});
 });
 
 app.get('/straitm', function(req, res) {
+	findDestination(req);
+
 	res.render('straitm', {pageName:location});
 });
 
 app.get('/guam', function(req, res) {
+	findDestination(req);
 	res.render('guam', {pageName:location});
 });
 
--- public/main.js
+++ public/main.js
@@ -1,5 +1,19 @@
-for(var i = 0; i < urlPaths.length; i++){
-	if(urlPaths[i] === app.get(urlPaths[i]) ) {
-		// ????
-	}
-}
+// figure out the current page url
+// find that entry in the urlPaths array
+// when user clicks next button (event handler), return the urlPaths + 1 in the url
+// (destinationPath = urlPaths[i] + 1)
+// send user to that page
+
+// var currentPage = req.path;
+// console.log(currentPage);
+// for(var i = 0; i < urlPaths.length; i++){
+// 	// console.log(urlPaths[i]);
+// 	if(urlPaths[i] === app.get(urlPaths[i]) ) {
+// 		// ??
+// 		currentPage = urlPaths[i];
+// 		destinationPath = currentPage ++;
+// 		return destinationPath;
+// 	}
+// 		// console.log(urlPaths[i]);
+
+// }
